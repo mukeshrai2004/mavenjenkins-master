e0a6260e502af8e25c53baadb3422c2a2240dbcb
Mukesh <mukeshrai2004@yahoo.co.in>
2025-03-30 21:13:01 +0530

    Added/Updated test cases

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.Assert;
+import org.testng.TestNG;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest 
--- src/test/java/mavenforjenkins/newclass.java
+++ src/test/java/mavenforjenkins/newclass.java
@@ -1,10 +1,20 @@
 package mavenforjenkins;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.TestNG;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class newclass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("hello world");
 	}
 
 }
